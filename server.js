7f504c62833df7ff184b5b10ac5b600ec7e54e42
Sam Peterson <samuel.e.peterson@gmail.com>
2016-10-27 20:53:32 -0400

    add initial scaffolding

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+process.env.ENV = process.env.ENV || 'dev';
+
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(morgan('dev'));
+
+if (process.env.ENV === 'dev') {
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    stats: {
+      colors: true,
+      chunks: false,
+    },
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(express.static(path.join(__dirname, '/dist')));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// app.use('/newSignup', SIGNUP ROUTER);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`LISTENING on Port ${port}`);
+});
