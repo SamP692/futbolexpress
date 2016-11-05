fab1538de5a5fa23a2ef2ba6a3c1f5d73cbc3e9a
Sam Peterson <samuel.e.peterson@gmail.com>
2016-11-05 17:31:37 -0400

    add request for list of teams inside league component

--- fantasyWithExpress/client/components/League.jsx
+++ fantasyWithExpress/client/components/League.jsx
@@ -11,9 +11,27 @@ class League extends Component {
     super();
     this.state = {
       leagueName: '',
+      teamOne: {},
+      teamTwo: {},
+      teamThree: {},
+      teamFour: {},
+      teamFive: {},
+      teamSix: {},
+      teamSeven: {},
+      teamEight: {},
+      teamNine: {},
+      teamTen: {},
+      teamEleven: {},
+      teamTwelve: {},
     };
+    this.getLeagueInformation = this.getLeagueInformation.bind(this);
+    this.getTeamsInformation = this.getTeamsInformation.bind(this);
   }
   componentDidMount() {
+    this.getLeagueInformation();
+    this.getTeamsInformation();
+  }
+  getLeagueInformation() {
     request.get('/api/league/find')
            .then((res) => {
              this.setState({ leagueName: res.body.name });
@@ -21,6 +39,14 @@ class League extends Component {
            .catch(() => {
              this.props.router.push('/');
              console.log('you\'ve been pushed!');
+           });
+  }
+  getTeamsInformation() {
+    request.get('/api/team/findbyleague')
+           .then((res) => {
+             console.log(`Team List: ${res}`);
+           }).catch((err) => {
+             console.log(err);
            });
   }
   render() {
