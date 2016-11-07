d849493cac6ced3032282ea68d0c7edc9a5a82b3
Sam Peterson <samuel.e.peterson@gmail.com>
2016-11-06 23:54:08 -0500

    add general styling as well as content to home page

--- fantasyWithExpress/client/components/Home.jsx
+++ fantasyWithExpress/client/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Home extends Component {
   constructor() {
@@ -7,10 +8,33 @@ class Home extends Component {
   render() {
     return (
       <div id="homeBody">
-        <h1>Welcome to Futbol Express</h1>
+        <div className="header">
+          <h1>Fubol Express 2016</h1>
+          <hr />
+        </div>
+        <div className="content">
+          <Link to="about">
+            <div className="linkBox" id="top" to="/about">
+              learn more about <span className="title">Futbol Express</span>
+            </div>
+          </Link>
+          <Link to="login">
+            <div className="linkBox" id="left">
+              <h2>returning user?</h2>
+              <h3>get me back in the action</h3>
+            </div>
+          </Link>
+          <Link to="register">
+            <div className="linkBox" id="right">
+              <h2>new to <span className="title">Futbol Express?</span></h2>
+              <h3>set me up</h3>
+            </div>
+          </Link>
+        </div>
       </div>
     );
   }
 }
+
 
 export default Home;
